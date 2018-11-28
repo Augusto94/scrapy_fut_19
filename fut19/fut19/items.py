e47695241a9d4c53f47070a9a75d23548ca50906
augusto94 <arl4@cin.ufpe.br>
2018-11-28 02:14:31 -0300

    Adiciona items da spider

--- fut19/fut19/items.py
+++ fut19/fut19/items.py
@@ -6,9 +6,31 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy import Field
 
 
 class Fut19Item(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    _id = Field()
+    club = Field()
+    ligue = Field()
+    nationality = Field()
+    name = Field()
+    rating = Field()
+    position = Field()
+    pace = Field()
+    shoot = Field()
+    passe = Field()
+    dribble = Field()
+    defense = Field()
+    physicist = Field()
+    price = Field()
+    compared_yesterday = Field()
+    age = Field()
+    height = Field()
+    weight = Field()
+    workrates = Field()
+    foot = Field()
+    phisical_info = Field()
+    stats = Field()
