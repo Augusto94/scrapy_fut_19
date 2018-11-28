ff9a348b8e05eeff08e6349dcbcdd95fe1c84864
augusto94 <arl4@cin.ufpe.br>
2018-11-28 02:14:01 -0300

    Modifica pipeline para atualizar documents

--- fut19/fut19/pipelines.py
+++ fut19/fut19/pipelines.py
@@ -12,18 +12,36 @@ class Fut19Pipeline(object):
         return item
 
 
-class MongoDBPipeline(object):
+class MongoPipeline(object):
 
-    def __init__(self):
-        connection = pymongo.MongoClient(
-            settings['MONGODB_SERVER'],
-            settings['MONGODB_PORT']
+    collection_name = 'players_fut'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'players')
         )
-        db = connection[settings['MONGODB_DB']]
-        self.collection = db[settings['MONGODB_COLLECTION']]
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
 
     def process_item(self, item, spider):
-        self.collection.insert(dict(item))
-        log.msg("Item added to MongoDB database!",
+        item_player = dict(item)
+        if self.db[self.collection_name].find({'_id': item_player['_id']}).count() != 0:
+            del item_player['_id']
+            self.db[self.collection_name].update({'_id': dict(item)['_id']}, {"$set": item_player}, upsert=False)
+        else:
+            self.db[self.collection_name].insert_one(item_player)
+            log.msg("Player added to MongoDB database!", 
                 level=log.DEBUG, spider=spider)
+
         return item
