2553dddefd0b472070f412de5ab6f820ecef8bb4
augusto94 <arl4@cin.ufpe.br>
2018-11-28 02:15:14 -0300

    Adiciona pipeline no settings do projeto

--- fut19/fut19/settings.py
+++ fut19/fut19/settings.py
@@ -64,9 +64,9 @@ SPIDER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'fut19.pipelines.Fut19Pipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'fut19.pipelines.MongoPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
