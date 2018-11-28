2be921bc5229832410d20864bdf59543ac829ea8
augusto94 <arl4@cin.ufpe.br>
2018-11-28 02:16:46 -0300

    Implementa a raspagem parcial dos dados

--- fut19/fut19/spiders/players/players.py
+++ fut19/fut19/spiders/players/players.py
@@ -5,7 +5,8 @@ import scrapy
 
 from scrapy.http import Request
 
-from .constants import XPATH_LINKS, XPATH_PAGINATION
+from fut19.items import Fut19Item
+from .constants import XPATH_LINKS, XPATH_PAGINATION, XPATHS_PLAYER
 
 
 class PlayersSpider(scrapy.Spider):
@@ -46,7 +47,39 @@ class PlayersSpider(scrapy.Spider):
                 )
 
     def extract_info_player(self, response):
-        import ipdb; ipdb.set_trace()     
+        item = Fut19Item()
+        
+        item['_id'] = int(response.url.split('/')[-1])
+
+        title = response.xpath(XPATHS_PLAYER['title'])
+        item['club'] = title.xpath(XPATHS_PLAYER['club']).extract_first()
+        item['ligue'] = title.xpath(XPATHS_PLAYER['ligue']).extract_first()
+        item['nationality'] = title.xpath(XPATHS_PLAYER['nationality']).extract_first()
+
+        item['name'] = response.xpath(XPATHS_PLAYER['name']).extract_first()
+        item['rating'] = response.xpath(XPATHS_PLAYER['rating']).extract_first()
+        item['position'] = response.xpath(XPATHS_PLAYER['position']).extract_first()
+        item['price'] = response.xpath(XPATHS_PLAYER['price']).extract_first()
+        item['compared_yesterday'] = response.xpath(XPATHS_PLAYER['compared_yesterday']).extract_first()
+        item['stats'] = {
+            'pace': response.xpath(XPATHS_PLAYER['pace']).extract_first(),
+            'shoot': response.xpath(XPATHS_PLAYER['shoot']).extract_first(),
+            'passe': response.xpath(XPATHS_PLAYER['passe']).extract_first(),
+            'dribble': response.xpath(XPATHS_PLAYER['dribble']).extract_first(),
+            'defense': response.xpath(XPATHS_PLAYER['defense']).extract_first(),
+            'physicist': response.xpath(XPATHS_PLAYER['physicist']).extract_first(),
+        }
+
+        phisical_info = response.xpath(XPATHS_PLAYER['phisical_info'])
+        item['phisical_info'] = {
+            'age': phisical_info.xpath(XPATHS_PLAYER['age']).extract_first(),
+            'height': phisical_info.xpath(XPATHS_PLAYER['height']).extract_first(),
+            'weight': phisical_info.xpath(XPATHS_PLAYER['weight']).extract_first(),
+            'workrates': phisical_info.xpath(XPATHS_PLAYER['workrates']).extract_first(),
+            'foot': phisical_info.xpath(XPATHS_PLAYER['foot']).extract_first(),
+        }
+
+        yield item
 
 
 # with open('dump.html', 'wb') as f: f.write(response.body)
