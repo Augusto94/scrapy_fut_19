ba5fbb2871694f4813e2808e7b2594207b092200
augusto94 <arl4@cin.ufpe.br>
2018-11-28 14:27:17 -0300

    Separa xpaths e adiciona mais xpaths de dados da página

--- fut19/fut19/spiders/players/constants.py
+++ fut19/fut19/spiders/players/constants.py
@@ -6,23 +6,35 @@ XPATH_LINKS = ('//tr[contains(@class, "table-row")]'
 XPATH_PAGINATION = ('//div[contains(@class, "pagination")]'
                     '//div[contains(@class, "next")]//a/@href')
 
-XPATHS_PLAYER = {
-    'title': '//div[@class="titleDetails"]',
+XPATHS_PLAYER_INFO = {
+    '_title': '//div[@class="titleDetails"]',
+    '_physical_info': '//div[@class="physicalInfo"]',
     'club': './/div[contains(@class, "col-4")][1]/a/text()',
     'ligue': './/div[contains(@class, "col-4")][2]/a/text()',
     'nationality': './/div[contains(@class, "col-4")][3]/a/text()',
     'name': '//div[contains(@class, "name")][1]/text()',
     'rating': '//div[contains(@class, "-rating")]/text()',
     'position': '//div[contains(@class, "-position")]/text()',
+    'skills': 'count(//div[contains(@class, "left")]/div[@class="stars"]/*)',
+    'weakfoot': 'count(//div[contains(@class, "right")]/div[@class="stars"]/*)',
+    'price': '//span[@class="counter"]/text()',
+    'compared_yesterday': '//span[@class="green"]/text()',
+    'workrates_info': ('//div[@class="physicalInfo"]'
+                       '//div[contains(@class, "col")][5]/text()[2]'),
+    'foot_info': ('//div[@class="physicalInfo"]'
+                  '//div[contains(@class, "col")][6]/text()[2]'),
+}
+
+XPATHS_STATS_INFO = {
     'pace': '//div[@class="card-19-attnum card-19-attnum1"]/text()',
     'shoot': '//div[@class="card-19-attnum card-19-attnum2"]/text()',
     'passe': '//div[@class="card-19-attnum card-19-attnum3"]/text()',
     'dribble': '//div[@class="card-19-attnum card-19-attnum4"]/text()',
     'defense': '//div[@class="card-19-attnum card-19-attnum5"]/text()',
     'physicist': '//div[@class="card-19-attnum card-19-attnum6"]/text()',
-    'price': '//span[@class="counter"]/text()',
-    'compared_yesterday': '//span[@class="green"]/text()',
-    'phisical_info': '//div[@class="physicalInfo"]',
+}
+
+XPATHS_PHYSICAL_INFO = {
     'age': './/div[contains(@class, "col")][1]/text()[2]',
     'height': './/div[contains(@class, "col")][3]/text()[2]',
     'weight': './/div[contains(@class, "col")][4]/text()[2]',
